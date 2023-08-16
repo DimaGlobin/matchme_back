a1880b4b528ef235a6ad2efffd2f990bde87c331
DimaGlobin <globin.do@phystech.edu>
2023-08-17 02:28:20 +0300

    Minor changes

--- models/userModel.go
+++ models/userModel.go
@@ -6,7 +6,7 @@ type User struct {
 	gorm.Model
 	Email            string `gorm:"unique"`
 	Password         string
-	Sex              string `gorm:"type:VARCHAR(10) CHECK(sex IN ('male', 'female'))"`
+	Sex              string
 	Location         string
 	Description      string `gorm:"type:TEXT"`
 	liked            []int  `gorm:"type:integer[]"`
